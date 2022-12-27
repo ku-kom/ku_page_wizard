657c742e1e28095dfe21beb64dbbbb8f8626d156
nanna-dk <nanna@e-nanna.dk>
2022-12-27 17:25:49 +0100

    Setup

--- ext_tables.php
+++ ext_tables.php
@@ -1,16 +1,18 @@
 <?php
 
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule(
-        'web',
-        'txkupagewizard',
-        'after:web_layout',
-        null,
-        [
-            'navigationComponentId' => 'TYPO3/CMS/Backend/PageTree/PageTreeElement',
-            'routeTarget' => \UniversityOfCopenhagen\KuPageWizard\Controller\PageWizardController::class . '::handleRequest',
-            'access' => 'user,group',
-            'name' => 'web_txkupagewizard',
-            'iconIdentifier' => 'ku-page-wizard-backend-module',
-            'labels' => 'LLL:EXT:ku_page_wizard/Resources/Private/Language/Module/locallang_mod.xlf'
-        ]
-    );
+defined('TYPO3') or die('Access denied.');
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule(
+    'web',
+    'txkupagewizard',
+    'after:web_layout',
+    null,
+    [
+        'navigationComponentId' => 'TYPO3/CMS/Backend/PageTree/PageTreeElement',
+        'routeTarget' => \UniversityOfCopenhagen\KuPageWizard\Controller\PageWizardController::class . '::handleRequest',
+        'access' => 'user,group',
+        'name' => 'web_txkupagewizard',
+        'iconIdentifier' => 'ku-page-wizard-backend-module',
+        'labels' => 'LLL:EXT:ku_page_wizard/Resources/Private/Language/Module/locallang_mod.xlf'
+    ]
+);
